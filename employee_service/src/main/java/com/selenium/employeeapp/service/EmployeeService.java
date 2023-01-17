f39d0b8e658f5897ba5b2e8fe2621403e6b3c822
bhavya-maguluri <66251102+bhavya-maguluri@users.noreply.github.com>
2023-01-17 15:35:51 +0530

    Add files via upload
    In this, modelmapper is used for fetching the data from db

--- employee_service/src/main/java/com/selenium/employeeapp/service/EmployeeService.java
+++ employee_service/src/main/java/com/selenium/employeeapp/service/EmployeeService.java
@@ -1,17 +1,31 @@
 package com.selenium.employeeapp.service;
 
+
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.selenium.employeeapp.entity.Employee;
 import com.selenium.employeeapp.repo.EmployeeRepo;
+import com.selenium.employeeapp.response.EmployeeResponse;
 @Service
 public class EmployeeService {
 	@Autowired
 	private EmployeeRepo EmployeeRepo;
 	
-	public Employee getEmployeeById(int id){
-		Employee employee = EmployeeRepo.findById(id).get();
-		return employee;
-	}
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	public EmployeeResponse getEmployeeById(int id) {
+		
+		Employee employee=EmployeeRepo.findById(id).get();
+		EmployeeResponse employeeResponse= modelMapper.map(employee,EmployeeResponse.class);
+		//EmployeeResponse employeeResponse = new EmployeeResponse();
+		//employeeResponse.setId(employee.getId());
+		//employeeResponse.setName(employee.getName());
+		//employeeResponse.setEmail(employee.getEmail());
+		//employeeResponse.setBloodGroup(employee.getBloodGroup());
+		return employeeResponse;
+	
+}
 }
